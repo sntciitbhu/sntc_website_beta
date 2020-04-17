823af330d1fbe88dabba0972bd5ae44f0c9e950f
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-17 19:26:04 +0530

    added sections list for refernce

--- apps/technex/views.py
+++ apps/technex/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def technex(request):
+    sections = [
+        'PR - (Talks and exhibitions)',
+        "Events",
+        'workshops',
+        "chota sa kelaidoscope"
+    ]
     return HttpResponse('hello')
